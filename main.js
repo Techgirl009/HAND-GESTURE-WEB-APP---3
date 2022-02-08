b3289dff825068e60dac9ca5864f64de382afeb7
Techgirl009 <89428705+Techgirl009@users.noreply.github.com>
2022-02-07 19:37:47 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,96 @@
+Webcam.set({
+    width:298,
+    height:298,
+    image_format:'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'">';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/yTY7RXP8p/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log('ModelLoaded!');
+}
+
+prediction_1 = "";
+prediction_2 = "";
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is " + prediction_1;
+    speak_data_2 = "The second prediction is " + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_hand_name1").innerHTML = results[0].label;
+        document.getElementById("result_hand_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if(results[0].label == "amazing")
+        {
+            document.getElementById("update_hand1").innerHTML = "&#128076;";
+        }
+
+        if(results[0].label == "hashtag")
+        {
+            document.getElementById("update_hand1").innerHTML = "&#9996;";
+        }
+
+        if(results[0].label == "thumbsup")
+        {
+            document.getElementById("update_hand1").innerHTML = "&#128077;";
+        }
+
+        if(results[0].label == "thumbsdown")
+        {
+            document.getElementById("update_hand1").innerHTML = "&#128078;";
+        }
+
+
+        if(results[1].label == "amazing")
+        {
+            document.getElementById("update_hand2").innerHTML = "&#128076;";
+        }
+
+        if(results[1].label == "hashtag")
+        {
+            document.getElementById("update_hand2").innerHTML = "&#9996;";
+        }
+
+        if(results[1].label == "thumbsup")
+        {
+            document.getElementById("update_hand2").innerHTML = "&#128077;";
+        }
+
+        if(results[1].label == "thumbsdown")
+        {
+            document.getElementById("update_hand2").innerHTML = "&#128078;";
+        }
+    }
+}
